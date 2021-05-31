7757aaa34a71833491d82a67807383db1703a6f1
P <69576826+phatnguyen1006@users.noreply.github.com>
2021-05-31 14:40:07 +0700

    Uploads Code to Import

---
+++ Import.sql
@@ -0,0 +1,12 @@
+use QLSACH
+go
+-- Script to import before export
+--- Import TACGIA
+INSERT INTO TACGIA VALUES('TG01',N'Nguyễn Thụy Quỳnh Trang','0906762722','tg01@gmail.com')
+INSERT INTO TACGIA VALUES('TG02',N'Nguyễn Văn An','0906762255','tg02@gmail.com')
+INSERT INTO TACGIA VALUES('TG03',N'Nguyễn Thúy Vi','090671234','tg03@gmail.com')
+INSERT INTO TACGIA VALUES('TG04',N'Phan Lưu Đăng','090671234','tg04@gmail.com')
+INSERT INTO TACGIA VALUES('TG05',N'Nguyễn Thanh Thảo','0910122201','tg05@gmail.com')
+---
+go
+use master
